34f5ae760e54bf44f6dde54be09cb47555398f0c
Yudis <yfran022@gmail.com>
2026-03-12 12:58:43 -0500

    punto
    Refactor to remove analizar_numeros function and add product input handling.

--- punto 5.py
+++ punto 5.py
@@ -1,21 +1,24 @@
-def analizar_numeros(*args, **kwargs):
+import os
+os.system("cls")  
 
- pares = []
- impares = []
+while true:
+    nombre = input("ingrese nombre del producto(fin para terminar): ")
+    if nombre.lower()=="fin":
+       break
+    precio = float(input("ingrese el precio del producto: "))
+    producto = {
+      "nombre"= nombre,
+      "precio" = precio
+    }
+    productos.append(producto)
+ 
+def total_productos(lista):
+   return len(lista)
 
-for n in args:
+def precio_promedio
+ sum = 0
+ for x in lista.
+  suma +- = x["precio"]
+ return suma / len(lista)
 
- if n % 2 == 0:
- pares.append(n)
-else:
-  impares.append(n)
-
-if kwargs.get("mostrar_pares") == True:
- print("Numeros pares:", pares, "Total:", len(pares))
-
-if kwargs.get("mostrar_impares") == True:
- print("Numeros impares:", impares, "Total:", len(impares))
-
-
-# prueba
-analizar_numeros(1,2,3,4,5,6, mostrar_pares=True) 
+def producto_mas_caro
